f4d0e2231f3d1f9fad93de8d1fd8414fc8ee6239
CJM <whdals9514@naver.com>
2022-10-25 09:20:11 +0900

    third commit

--- app/src/main/java/com/cjm/picture_app/MainActivity.java
+++ app/src/main/java/com/cjm/picture_app/MainActivity.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.res.Resources;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ScrollView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -20,8 +23,6 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<Integer> IMGS = new ArrayList<>();
     int pic_num, now_img;
     BitmapDrawable bitmap;
-    EditText editText;
-
     //    endregion
 
     @Override
@@ -29,22 +30,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //  region Create then Init
         Resources res = getResources();
         for (int i = 0; i < 30; i++) {
             String img_name = "image" + (i + 1);
             IMGS.add(res.getIdentifier(img_name, "drawable", getPackageName()));
         }
+        //  endregion
 
-//        region  Id declare
+        // region Id declare
         ImageView imageView = (ImageView) findViewById(R.id.imageView);
-        editText = (EditText) findViewById(R.id.editText);
+        EditText editText = (EditText) findViewById(R.id.editText);
         Button button_prev = (Button) findViewById(R.id.button_prev);
         Button button_next = (Button) findViewById(R.id.button_next);
-//        endregion
+        // endregion
 
-//        region Initial setting
-
-//        bitmap = (BitmapDrawable) res.getDrawable(R.drawable.image1);
+        // region Initial setting
         bitmap = (BitmapDrawable) res.getDrawable(IMGS.get(0));
         now_img = 1;
         int bitmapWidth = bitmap.getIntrinsicWidth();
@@ -52,8 +53,81 @@ public class MainActivity extends AppCompatActivity {
         imageView.setImageDrawable(bitmap);
         imageView.getLayoutParams().width = bitmapWidth;
         imageView.getLayoutParams().height = bitmapHeight;
+        // endregion
 
-//        endregion
+        // region Button Function
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+
+                int text = (int) Math.round(Double.parseDouble(textView.getText().toString()));
+
+                switch (i) {
+                    // Search button
+                    case android.view.inputmethod.EditorInfo.IME_ACTION_SEARCH:
+                        Toast.makeText(MainActivity.this, "Search button", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    // Enter button
+                    default:
+                        if(text == 0){
+                            Toast.makeText(MainActivity.this, "사진은 1~30까지만 있습니다.", Toast.LENGTH_SHORT).show();
+                            now_img = 1;
+                            editText.setText(Integer.toString(now_img));
+                            bitmap = (BitmapDrawable) res.getDrawable(IMGS.get(now_img-1));
+                            int bitmapWidth = bitmap.getIntrinsicWidth();
+                            int bitmapHeight = bitmap.getIntrinsicHeight();
+                            imageView.setImageDrawable(bitmap);
+                            imageView.getLayoutParams().width = bitmapWidth;
+                            imageView.getLayoutParams().height = bitmapHeight;
+                            imageView.setImageDrawable(bitmap);
+                        }else  if(text < 1){
+                            Toast.makeText(MainActivity.this, "사진은 1~30까지만 있습니다.", Toast.LENGTH_SHORT).show();
+                            now_img = 1;
+                            editText.setText(Integer.toString(now_img));
+                            bitmap = (BitmapDrawable) res.getDrawable(IMGS.get(now_img-1));
+                            int bitmapWidth = bitmap.getIntrinsicWidth();
+                            int bitmapHeight = bitmap.getIntrinsicHeight();
+                            imageView.setImageDrawable(bitmap);
+                            imageView.getLayoutParams().width = bitmapWidth;
+                            imageView.getLayoutParams().height = bitmapHeight;
+                            imageView.setImageDrawable(bitmap);
+                        }else if(text >30){
+                            Toast.makeText(MainActivity.this, "사진은 1~30까지만 있습니다.", Toast.LENGTH_SHORT).show();
+                            now_img = 30;
+                            editText.setText(Integer.toString(now_img));
+                            bitmap = (BitmapDrawable) res.getDrawable(IMGS.get(now_img-1));
+                            int bitmapWidth = bitmap.getIntrinsicWidth();
+                            int bitmapHeight = bitmap.getIntrinsicHeight();
+                            imageView.setImageDrawable(bitmap);
+                            imageView.getLayoutParams().width = bitmapWidth;
+                            imageView.getLayoutParams().height = bitmapHeight;
+                            imageView.setImageDrawable(bitmap);
+                        }else {
+                            now_img = text;
+                            editText.setText(Integer.toString(now_img));
+                            bitmap = (BitmapDrawable) res.getDrawable(IMGS.get(now_img-1));
+                            int bitmapWidth = bitmap.getIntrinsicWidth();
+                            int bitmapHeight = bitmap.getIntrinsicHeight();
+                            imageView.setImageDrawable(bitmap);
+                            imageView.getLayoutParams().width = bitmapWidth;
+                            imageView.getLayoutParams().height = bitmapHeight;
+                            imageView.setImageDrawable(bitmap);
+                        }
+
+                        return false;
+                }
+
+                // reset
+                textView.clearFocus();
+                textView.setFocusable(false);
+                textView.setText("");
+                textView.setFocusableInTouchMode(true);
+                textView.setFocusable(true);
+
+                return true;
+            }
+        });
 
         button_prev.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -92,7 +166,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
+        // endregion
 
     }
 }
